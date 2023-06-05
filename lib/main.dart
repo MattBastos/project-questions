95eb1001eb4924d88d7061df1ddc31b7792fff4b
Matthews Bastos <matthewsbasttos@gmail.com>
2023-06-05 12:21:24 -0300

    build: Começando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const QuestionsApp());
+
+class QuestionsApp extends StatelessWidget {
+  const QuestionsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Questions'),
+        ),
+        body: const Text('Hello World!!!'),
+      ),
+    );
+  }
+}
