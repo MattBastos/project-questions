c1982a32fc94b636dec869c8888b8249c33f852f
Matthews Bastos <matthewsbasttos@gmail.com>
2023-06-05 15:37:06 -0300

    feat: Implementa o método answer na classe QuestionsAppState

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,21 @@ import 'package:flutter/material.dart';
 main() => runApp(const QuestionsApp());
 
 class QuestionsAppState extends State<QuestionsApp> {
-  final List<String> questions = [
-    "What's your favorite video game?",
-    "What's your favorite movie?",
-  ];
+  var selectedQuestion = 0;
+
+  final List<String> answers = [];
+
+  void answer() {
+    setState(() => selectedQuestion += 1);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final List<String> questions = [
+      "What's your favorite video game?",
+      "What's your favorite movie?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,21 +25,15 @@ class QuestionsAppState extends State<QuestionsApp> {
         ),
         body: Column(
           children: [
-            Text(questions[0]),
+            Text(questions[selectedQuestion]),
             const TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
               ),
             ),
-            Text(questions[1]),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text('Answer')
+            ElevatedButton(
+              onPressed: answer,
+              child: const Text('Answer')
             ),
           ],
         ),
